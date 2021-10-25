ef162ec29b7e86e55a756741088049822eeb92ac
TFoley <91576580+TFoley1@users.noreply.github.com>
2021-10-25 09:38:27 -0400

    12 buttons Js
    Added code to make my 12 buttons work correctly.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let button7Clicks = 0;
+
+
+function changeButtons() {
+  document.getElementById('button8').style.color = "red";
+  document.getElementById("button8").style.fontSize = "20px";
+  button7Clicks = button7Clicks + 1;
+  if (button7Clicks > 1 ) {
+    document.getElementById("button8").innerHTML = "*ANGRY BUTTON NOISE*";
+    document.getElementById("button8").style.width = "90px";
+  }
+}
+
+function displayPrompt() {
+  let  answer = prompt("Name your favorite button so far!");
+
+  document.getElementById("answer").innerHTML = "You like " + answer + " ?";
+}
 
